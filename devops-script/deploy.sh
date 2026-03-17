f22d3c3aa13e9eed41710699105fa993db1c78a0
Biswarup <biswarupghosh520@gmail.com>
2026-03-17 16:39:58 +0530

    pipeline working 1

--- devops-script/deploy.sh
+++ devops-script/deploy.sh
@@ -20,7 +20,7 @@ docker rm $CONTAINER_NAME || true
 docker run -d \
   --name $CONTAINER_NAME \
   --env-file $ENV_FILE \
-  -p 3000:3000 \
+  -p 80:3000 \
   biswarup007/node-app:$IMAGE_TAG
 
 echo "Deployment complete: $CONTAINER_NAME running with image $IMAGE_TAG"
