ac4c9792fb4f5f83dfedac2ad6a947e76119666b
Biswarup <biswarupghosh520@gmail.com>
2026-03-17 17:08:23 +0530

    pipeline working 2

--- devops-script/deploy.sh
+++ devops-script/deploy.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 # deploy.sh
 
-# Exit on any error
 set -e
 
-# Variables
-IMAGE_TAG=$1              # Pass the image tag (short SHA) as argument
+IMAGE_TAG=$1              
 CONTAINER_NAME="node-app"
-ENV_FILE="/home/ec2-user/.env"  # Path to your .env file on EC2
+ENV_FILE="/home/ec2-user/.env"  
 
-# Pull the latest image from Docker Hub
 docker pull biswarup007/node-app:$IMAGE_TAG
 
-# Stop and remove old container if it exists
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
-# Run the new container
 docker run -d \
   --name $CONTAINER_NAME \
   --env-file $ENV_FILE \
